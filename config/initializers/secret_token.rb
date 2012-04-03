ad72a438672931487548955aa3e7f8553d87de2a
railgun <inso37@gmail.com>
2012-04-03 19:16:30 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gayshit::Application.config.secret_token = '4fbd345506c4f3a91258a7e33bc55502cf5865ba55691cb564abecd64c69b53feda2c22c985a8f34693fc14655bec406b630c2f71eb60eeb8578ab3c7134172a'
